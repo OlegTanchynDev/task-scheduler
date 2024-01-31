c69f071e5e5abc4f64524a9f90f6bafd88228280
Oleh Tanchyn <tankerman.oleh@gmail.com>
2024-01-31 10:30:40 +0200

    Added authentication with Device gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Device
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'secret',
+    confirmation: 'verification',
+    registration: 'register',
+    sign_up: 'signup'
+  }
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: 'rails/health#show'
 end
